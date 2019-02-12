0672af90927e4ea3c1df2ec26967d2dd73be9c83
Kunal <kunaljha5@gmail.com>
2019-02-13 00:03:51 +0530

    Update report_final.sh

--- report_final.sh
+++ report_final.sh
@@ -17,7 +17,7 @@ cat report_*csv| cut -d, -f1|sort| uniq | while read line ;
 do
 # starting the for loop below for each report_*csv file i.e report_dit1.csv , report_dit2.csv report_sit1.csv 
 
-        for i in `ls  report_*csv | grep -v report_final.csv`;
+        for i in `ls  report_*csv | grep -v final_report.csv`;
         do
                 # This step verifies that x module is present on which env and if its not present it will be placed as - in final csv file. 
                 cat $i | grep "$line" 2>/dev/null 1>/dev/null
@@ -46,18 +46,18 @@ root_master_compare()
 
 #Comparision between source and Destination Env for module version 
 
-# setting the DEST variable with  cloumn number for Dest env in report_final.csv
+# setting the DEST variable with  cloumn number for Dest env in final_report.csv
 
-DEST=$(cat   report_final.csv |grep 'module-name'| tr ',' "\n"| nl | grep "$dest_node"| awk '{print $1}')
+DEST=$(cat   final_report.csv |grep 'module-name'| tr ',' "\n"| nl | grep "$dest_node"| awk '{print $1}')
 
-# setting the Source variable with  cloumn number for Dest env in report_final.csv
-SOURCE=$(cat   report_final.csv |grep 'module-name'| tr ',' "\n"|nl | grep "$src_node"| awk '{print $1}')
+# setting the Source variable with  cloumn number for Dest env in final_report.csv
+SOURCE=$(cat   final_report.csv |grep 'module-name'| tr ',' "\n"|nl | grep "$src_node"| awk '{print $1}')
 
 
 # report_final1.csv file is being created like  module1,1.1.10
-cat report_final.csv | grep -v "module-name"|cut -d, -f1,$SOURCE >  report_final1.csv
+cat final_report.csv | grep -v "module-name"|cut -d, -f1,$SOURCE >  report_final1.csv
 # report_final1.csv file is being created like  1.1.10
-cat report_final.csv | grep -v "module-name"|cut -d, -f$DEST >  report_final2.csv
+cat final_report.csv | grep -v "module-name"|cut -d, -f$DEST >  report_final2.csv
 
 # Now we are putting above to files content side by side and now they will appear like "module1,1.1.10,1.1.10"  in Report_finalR.csv
 
@@ -106,10 +106,10 @@ rm -rf Report_finalR.csv
 
 
 
-root_final > report_final.csv
+root_final > final_report.csv
 if [[ $? -eq 0 ]]
 then
-        echo "Final Report file stored at report_final.csv"
+        echo "Final Report file stored at final_report.csv"
 else
         echo "Process of Final Report file failed."
 fi
@@ -120,6 +120,6 @@ src_node=$2
 
 
 
-root_master_compare > Report_filter.csv
+root_master_compare > Filter_Report.csv
 
 
